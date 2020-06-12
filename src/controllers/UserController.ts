d1b12238812d47b4962fc3558ee0dc29934ccbd3
obraia <bryan22czo@gmail.com>
2020-06-11 21:04:09 -0300

    [feature] UserController configurado

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,59 @@
+// eslint-disable-next-line no-unused-vars
+import { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+import bcrypt from 'bcrypt';
+
+import knex from '../database/connection';
+
+dotenv.config();
+
+class UserController {
+  async create (req: Request, res: Response) {
+    const { username, password } = req.body;
+
+    const user = await knex('users')
+      .where('username', username)
+      .select('username')
+      .first();
+
+    if (user) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
+
+    const saltRounds = parseInt(String(process.env.SALT_ROUNDS));
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+
+    await knex('users').insert({
+      username,
+      password: hash
+    });
+
+    return res.status(201).json({ message: 'success' });
+  }
+
+  async update (req: Request, res: Response) {
+    return res.status(501).json({ message: 'Not implemented' });
+  }
+
+  async delete (req: Request, res: Response) {
+    const { username } = req.params;
+
+    const user = await knex('users')
+      .where('username', username)
+      .select('username')
+      .first();
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    await knex('users').where('username', username).del();
+
+    return res.status(201).json({ message: 'success' });
+  }
+}
+
+export default UserController;
