97a303e3fa7d9a783cc4da6da8ee61f5a1e87bf1
obraia <bryan22czo@gmail.com>
2020-06-11 21:02:35 -0300

    [feature] Rotas configuradas

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 
+import UserController from './controllers/UserController';
+import SessionController from './controllers/SessionController';
+import AuthMiddleware from './middlewares/AuthMiddleware';
+
 const routes = express.Router();
 
-routes.get('/', () => {});
+const userController = new UserController();
+const sessionController = new SessionController();
+const authMiddleware = new AuthMiddleware();
+
+routes.delete('/users/:username', authMiddleware.authenticate, userController.delete);
+routes.post('/register', userController.create);
+routes.post('/authenticate', sessionController.create);
 
 export default routes;
